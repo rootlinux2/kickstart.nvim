271e4eded8a3a6a80f06aa3199bd2f765377e5e3
Yoandry Collazo <ycbarrosocu@gmail.com>
2025-07-13 21:11:07 -0600

    Add: todo-comment pluggin

---
+++ lua/custom/plugins/todo-comment.lua
@@ -0,0 +1,12 @@
+return {
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('todo-comments').setup()
+  end,
+  keys = {
+    { '<leader>xt', '<cmd>TodoTrouble<CR>', desc = 'Todo (Trouble)' },
+    { '<leader>xT', '<cmd>TodoTrouble keywords=TODO,FIX,FIXME<CR>', desc = 'Todo/Fix/Fixme (Trouble)' },
+    { '<leader>st', '<cmd>TodoTelescope<CR>', desc = 'Todo' },
+  },
+}
